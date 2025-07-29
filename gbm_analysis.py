565b779bf89c0ced67d9381f7caa7458f6de76e0
Marcin Anforowicz <manforowicz@gmail.com>
2025-07-29 10:11:21 -0700

    Kelly

--- gbm_analysis.py
+++ gbm_analysis.py
@@ -9,13 +9,18 @@ from package import tickers
 from package.fetcher import StartDateMissing, get_inflation_adjusted_monthly
 
 
-def get_gbm_quantile(price_data: np.ndarray, points_per_year: int, quantile: float, years: float) -> float:
+def get_gbm_parameters(
+    price_data: np.ndarray, points_per_year: int
+) -> tuple[float, float]:
     log_returns = np.diff(np.log(price_data))
 
     # Annualize parameters (12 months a year)
     mu = log_returns.mean() * points_per_year
     sigma = log_returns.std(ddof=1) * np.sqrt(points_per_year)
+    return mu, sigma
 
+
+def get_gbm_quantile(mu: float, sigma: float, quantile: float, years: float) -> float:
     # Z-score for quantile in a standard normal distribution
     z = stats.norm.ppf(quantile)
 
@@ -33,25 +38,13 @@ def get_gbm_quantile(price_data: np.ndarray, points_per_year: int, quantile: flo
     # S_T / S_0 = exp(log_return)
     percent_change = np.exp(log_return_quantile) - 1
 
-    ###### PLOT
-    # # Step 3: Plot histogram of the data
-    # count, bins, _ = plt.hist(log_returns, bins=20, density=True, alpha=0.6, color='skyblue', label='Data histogram')
-
-    # # Step 4: Overlay the lognormal PDF
-    # x = np.linspace(min(log_returns), max(log_returns), 1000)
-    # pdf = stats.norm.pdf(x, loc=mu, scale=sigma)
-    # plt.plot(x, pdf, 'r-', lw=2, label='Lognormal PDF')
-
-    # # Final touches
-    # plt.xlabel('Value')
-    # plt.ylabel('Density')
-    # plt.title('Histogram with Lognormal Overlay')
-    # plt.legend()
-    # plt.grid(True)
-    # plt.show()
-
-
     return percent_change
+
+
+def get_kelly_fraction(mu: float, sigma: float) -> float:
+    r = 0.0
+
+    return (mu - r) / sigma**2
 
 
 if __name__ == "__main__":
@@ -66,13 +59,16 @@ if __name__ == "__main__":
             print(f"Skipping {ticker} because {e}")
             continue
 
-        low = get_gbm_quantile(fund_data.to_numpy(), 12, 0.1, 20)
-        high = get_gbm_quantile(fund_data.to_numpy(), 12, 0.5, 20)
+        mu, sigma = get_gbm_parameters(fund_data.to_numpy(), 12)
 
-        results.append((fund_data.name, low, high))
+        low = get_gbm_quantile(mu, sigma, 0.1, 20)
+        high = get_gbm_quantile(mu, sigma, 0.5, 20)
+        kelly_fraction = get_kelly_fraction(mu, sigma)
+
+        results.append((fund_data.name, low, high, kelly_fraction))
 
     results.sort(key=lambda x: x[1])
     results.reverse()
 
-    for (name, low, high) in results:
-        print(f"{low:.3%} -- {high:.3%}: {name}")
+    for name, low, high, kelly_fraction in results:
+        print(f"{low:.3%} -- {high:.3%} (f*: {kelly_fraction:.3%}): {name}")
